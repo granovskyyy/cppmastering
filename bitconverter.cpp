3881c29f9010521ed30bae6e38839755c1a452a4
tgranows <tgranowski@student.agh.edu.pl>
2025-10-06 13:35:46 +0200

    bin2dec

---
+++ bitconverter.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string bin = " ";
+    int num;
+    cout<<"Enter a number you want to convert to binary "<<endl;
+    cin>>num;
+    if(num==0)
+    {
+        cout<<0;
+    }
+    while(num>0)
+    {
+        int bit=num &1;
+        bin.push_back('0'+bit);
+        num>>=1;
+    }
+    reverse(bin.begin(),bin.end());
+    cout<<"Your num after binary conversion is equal "<<bin<<endl;
+
+}
